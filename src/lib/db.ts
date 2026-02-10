c1c1c167419d96bd01ae4d02c6680b27afe79d82
anirudhisonline <isanirudhonline@gmail.com>
2026-02-10 21:05:33 +0530

    default to deltas.sqlite

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-export const app_db = new Database(process.env.APP_DB_FILE_NAME!);
+export const app_db = new Database(process.env.APP_DB_FILE_NAME || "deltas.sqlite");
 export const db = drizzle({ client: app_db });
 export * as schema from "./db/schema";
 
