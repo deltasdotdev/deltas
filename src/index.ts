24c07a8922b3db3a681685b061ddb08cd6fa7fcf
anirudhisonline <isanirudhonline@gmail.com>
2026-02-10 21:00:30 +0530

    moved the files and folders around so it will be easy to import

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Elysia } from "elysia";
-import { runMigrations } from "./lib/db/migrate";
-import { initAuth } from "./lib/auth/init";
+import { runMigrations } from "./lib/db";
+import { initAuth } from "./lib/auth";
 import { test } from "./lib/mailer/test";
 
 await runMigrations();
 await initAuth();
 // await test();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
